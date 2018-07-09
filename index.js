9b6c3285dcb6d4fc8a68aadf5dd45cf18a285559
Stephen <smjarr89@gmail.com>
2018-07-09 14:28:33 -0400

    sent chat messages to all users

--- index.js
+++ index.js
@@ -13,14 +13,22 @@ const wss = new WebSocket.Server({ server: server });
 //  -Add event listeners
 //  -Listen for new connections
 wss.on('connection', (socket) => {
+    //When someone connects send the confirmation message
+    socket.send('Hey, you are now connected');
     //listen for messages on this socket
     socket.on('message', (msg) => {
         //convert plain text to json
-        msg = JSON.parse(msg);
+        // msg = JSON.parse(msg);
         console.log(msg);
-    })
+        //send messages back to all users
+        wss.clients.forEach((client) => {
+            client.send(msg);
+        });
+    });
 });
 //  -Test using wscat
+//  -installed globally - lets the CLI talk to a websocket server
+//  -With app running.. new terminal.. wscat -c ws://localhost:5000
 
 
 //listen on server rather than typically the express app since it ties the 2 together
